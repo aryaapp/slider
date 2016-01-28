8b3fdaaaddb0e081430f50c399b37a33b27b04be
Martin Frank <martin.frank.ts@gmail.com>
2016-01-28 12:45:01 +0100

    add merge function

--- src/Handle.jsx
+++ src/Handle.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import Tooltip from 'rc-tooltip';
 
+
+/**
+ * Overwrites obj1's values with obj2's and adds obj2's if non existent in obj1
+ * @param obj1
+ * @param obj2
+ * @returns obj3 a new object based on obj1 and obj2
+ */
+function mergeOptions(obj1, obj2) {
+  const obj3 = {};
+  for (const attrname in obj1) {
+    if (obj1.hasOwnProperty(attrname)) {
+      obj3[attrname] = obj1[attrname];
+    }
+  }
+  for (const attrname in obj2) {
+    if (obj2.hasOwnProperty(attrname)) {
+      obj3[attrname] = obj2[attrname];
+    }
+  }
+  return obj3;
+}
+
 export default class Handle extends React.Component {
   constructor(props) {
     super(props);
@@ -27,7 +49,7 @@ export default class Handle extends React.Component {
     const {className, tipTransitionName, tipFormatter, offset, value} = props;
     const {dragging, noTip, additionalHandleStyles} = props;
 
-    const style = Object.assign({ left: offset + '%' }, additionalHandleStyles);
+    const style = mergeOptions({ left: offset + '%' }, additionalHandleStyles);
     const handle = (<div className={className} style={style}
                       onMouseUp={this.showTooltip.bind(this)}
                       onMouseEnter={this.showTooltip.bind(this)}
--- src/Track.jsx
+++ src/Track.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
+/**
+ * Overwrites obj1's values with obj2's and adds obj2's if non existent in obj1
+ * @param obj1
+ * @param obj2
+ * @returns obj3 a new object based on obj1 and obj2
+ */
+function mergeOptions(obj1, obj2) {
+  const obj3 = {};
+  for (const attrname in obj1) {
+    if (obj1.hasOwnProperty(attrname)) {
+      obj3[attrname] = obj1[attrname];
+    }
+  }
+  for (const attrname in obj2) {
+    if (obj2.hasOwnProperty(attrname)) {
+      obj3[attrname] = obj2[attrname];
+    }
+  }
+  return obj3;
+}
+
 const Track = ({className, included, offset, length, additionalTrackStyles}) => {
-  const style = Object.assign({
+  const style = mergeOptions({
     left: offset + '%',
     width: length + '%',
     visibility: included ? 'visible' : 'hidden',
